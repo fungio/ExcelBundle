f48225a3ca293977b09cb7c8494438ed02ed7648
Pierrick Aubin <pierrick.aubin@siqual.fr>
2019-09-22 15:30:00 +0200

    Symfony 4 compatibility

--- DependencyInjection/LiuggioExcelExtension.php
+++ DependencyInjection/LiuggioExcelExtension.php
@@ -5,7 +5,7 @@ namespace Liuggio\ExcelBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -19,10 +19,8 @@ class LiuggioExcelExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $this->processConfiguration(new Configuration(), $configs);
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
