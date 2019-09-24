6e7fecf880ae14af994da4434a3f911986965ace
Pierrick Aubin <pierrick.aubin@siqual.fr>
2019-09-24 13:30:04 +0200

    Fix sf 4.3 root method deprecated in TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('fungio_excel');
+        $treeBuilder = new TreeBuilder('fungio_excel');
+        $treeBuilder->getRootNode();
 
         return $treeBuilder;
     }
