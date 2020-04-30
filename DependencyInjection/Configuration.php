49d77dbc4bd35feaf4763ee9869195f84b43ad00
Pierrick Aubin <pierrick.aubin@siqual.fr>
2020-04-30 17:27:04 +0200

    backward compatibility for sf 3.x

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('fungio_excel');
-        $treeBuilder->getRootNode();
+        $treeBuilder = new TreeBuilder();
+        $rootNode    = $treeBuilder->root('fungio_excel');
 
         return $treeBuilder;
     }
